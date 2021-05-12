e30a13144d71be7780c241f8a5925c9240461a57
Janeth <60449001+jymeraz@users.noreply.github.com>
2021-05-12 14:53:21 -0600

    Update Controller.dart

--- omokDart/lib/Controller.dart
+++ omokDart/lib/Controller.dart
@@ -1,9 +1,3 @@
-/// Name: Janeth Meraz
-/// Course: CS 3360
-/// Professor: Yoonsik Cheon
-/// TA: Ivan Gastelum
-/// Assignment: Omok Dart Project
-
 import 'ConsoleUI.dart';
 import 'WebClient.dart';
 import 'Model.dart';
