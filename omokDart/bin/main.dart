9a5ca85601f80e6f4d14fecfafab574066aad3f5
Janeth <60449001+jymeraz@users.noreply.github.com>
2021-05-12 14:52:55 -0600

    Update main.dart

--- omokDart/bin/main.dart
+++ omokDart/bin/main.dart
@@ -1,9 +1,3 @@
-/// Name: Janeth Meraz
-/// Course: CS 3360
-/// Professor: Yoonsik Cheon
-/// TA: Ivan Gastelum
-/// Assignment: Omok Dart Project
-
 import 'package:omokDart/Controller.dart';
 
 /// Call to the Controller to begin the game.
