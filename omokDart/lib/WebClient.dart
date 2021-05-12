37565c738abc065f32e5f556f99fe1d135d246bf
Janeth <60449001+jymeraz@users.noreply.github.com>
2021-05-12 14:54:15 -0600

    Update WebClient.dart

--- omokDart/lib/WebClient.dart
+++ omokDart/lib/WebClient.dart
@@ -1,9 +1,3 @@
-/// Name: Janeth Meraz
-/// Course: CS 3360
-/// Professor: Yoonsik Cheon
-/// TA: Ivan Gastelum
-/// Assignment: Omok Dart Project
-
 import 'dart:io';
 import 'package:http/http.dart' as http;
 import 'ResponseParser.dart';
